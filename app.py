30cc8cec1cb9d1d3b060d56d2f415cdbfff5c60b
manaleks <manaleksdev@gmail.com>
2019-05-24 21:06:14 +0300

    flask app

---
+++ app.py
@@ -0,0 +1,21 @@
+# -*- coding: utf8 -*-
+import os
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+def main_page():
+    return render_template('main.html')
+def simple_text(num):
+    return "Hello!" + str(num)
+
+rules = []
+rules.append({"rule":'/',"name":"main_page","method":main_page})
+rules.append({"rule":'/simple_text/<num>',"name":"simple_text","method":simple_text})
+for rule in rules:
+    app.add_url_rule(rule["rule"], rule["name"], rule["method"])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
