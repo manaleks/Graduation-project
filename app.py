9ad03af40a17ca760795c1e937f99a69392526ca
manaleks <manaleksdev@gmail.com>
2019-06-05 21:37:06 +0300

    html style transfer with tensorflow models

--- app.py
+++ app.py
@@ -1,49 +1,14 @@
 # -*- coding: utf8 -*-
-from sys import stdout
-import logging
-from flask import Flask, render_template, Response
-from flask_socketio import SocketIO
-
-from camera import Camera
-from painter import painter
+import os
+from flask import Flask, render_template
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.logger.addHandler(logging.StreamHandler(stdout))
-app.config['SECRET_KEY'] = 'secret!'    
-app.config['DEBUG'] = True
-socketio = SocketIO(app)
-camera = Camera(painter())
 
-
-@socketio.on('input image', namespace='/test')
-def test_message(input):
-    input = input.split(",")[1]
-    camera.enqueue_input(input)
-
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    app.logger.info("client connected")
-
-@app.route('/')
-def index():
-    """Video streaming home page."""
+# GAME
+@app.route('/', methods=['GET', 'POST'])
+def game():
     return render_template('index.html')
 
-
-def gen():
-    """Video streaming generator function."""
-    app.logger.info("starting to generate frames!")
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-@app.route('/video_feed')
-def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-if __name__ == '__main__':
-    socketio.run(app)
+if __name__ == "__main__":
+    app.run()
