dd7605db636ea40e81d43484612d558b8c52223b
Man <manaleksdev@gmail.com>
2019-06-17 17:56:35 +0300

    set styles and content

--- main.py
+++ main.py
@@ -109,6 +109,5 @@ def get_image(filename):
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
 if __name__ == "__main__":
     app.run()
