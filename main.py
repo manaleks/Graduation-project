bb7afbb54957a03d41bfe68c14203d939dcc7d5a
Man <manaleksdev@gmail.com>
2019-06-17 17:11:39 +0300

    favicon and html format

--- main.py
+++ main.py
@@ -104,5 +104,11 @@ def get_image(filename):
 
     return send_from_directory(BASE_FOLDER+'/static/images/', filename)
 
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 if __name__ == "__main__":
     app.run()
